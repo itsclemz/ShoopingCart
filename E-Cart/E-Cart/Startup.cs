2234f883f670f8a54454dea6034115de9d6adbf1
Isaac Clement <isaacclem7@gmail.com>
2020-06-09 05:36:08 +0100

    Project created successfully

---
+++ E-Cart/E-Cart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Cart.Startup))]
+namespace E_Cart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
